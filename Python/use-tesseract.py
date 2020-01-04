c63034d290c49eee608f88477f6da0211d573a86
Temurbek Sabirov <prodota150@gmail.com>
2020-01-04 15:48:12 -0500

    Implement OCR using python and command line

---
+++ Python/use-tesseract.py
@@ -0,0 +1,22 @@
+""""
+This code opens an image containing text, recognizes characters. If possible, it converts the image
+text into written text and prints it to the console.
+
+This is simple but yet great way to converting image into text.
+OCR is capable of reading the text on the given image.
+
+""""
+#Required to use Pillow to open an image
+from PIL import Image
+#Required to import pytesseract to convert the image text into string
+import pytesseract
+
+#Following line opens the image called 'text.png'
+image = Image.open("text.png")
+
+#The following line converts the image into text in english language
+text = pytesseract.image_to_string(image, lang = 'eng')
+
+#Following line prints the output to the console
+print(text)
+#printed text read "HAPPY NEW YEAR"
